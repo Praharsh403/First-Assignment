cc21f1da4d48b98d7852e12ec29530bf90047d6c
Praharsh403 <praharsh.403@gmail.com>
2026-02-14 19:30:10 +0530

    Delete Task 2.py

--- Task 2.py
+++
@@ -1,4 +0,0 @@
-FirstName= input("Enter your first name:" )
-LastName= input("Enter your last name:" )
-Full_Name= FirstName + " " + LastName + "!"
-print("Hello,",Full_Name, "Welcome to the Python Program.")
